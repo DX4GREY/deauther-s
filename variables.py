a76df6bb1ea816b7c7fbf3b3943a0d2ea4071970
DX4GREY <dxablack@gmail.com>
2025-11-25 23:00:10 +0700

    feat: update FOLDER_PATH to correct directory resolution

--- variables.py
+++ variables.py
@@ -1,5 +1,5 @@
 import os
 
 REQUIRED_CMDS = ["iw", "aireplay-ng", "ip", "mdk4", "xterm"]
-FOLDER_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)), "..")
+FOLDER_PATH = os.path.join(os.path.dirname(os.path.abspath(__file__)))
 SHORTCUT_PATH = "/usr/local/bin/deauther-s"
